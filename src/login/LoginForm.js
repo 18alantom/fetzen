db8a3e7d4d45977e9b535b3df512cbc8989e2e04
18alantom <2.alan.tom@gmail.com>
2019-08-02 21:12:43 +0530

    Complete login form code, posting of form will be done with backend

--- src/login/LoginForm.js
+++ src/login/LoginForm.js
@@ -2,26 +2,30 @@ import React from "react";
 import { withStyles } from "@material-ui/styles";
 import { fade, darken } from "@material-ui/core/styles/colorManipulator";
 import { Collapse, Link, Paper, Typography, TextField, Button } from "@material-ui/core";
+
+// Custom Text Field to change the default colors.
 const CustomTextField = withStyles(theme => ({
   root: {
     "& label": {
-      color: theme.palette.primary[900]
+      color: theme.palette.primary[800]
     },
-    "& label.Mui-focused": {
-      color: theme.palette.primary[900]
+    "& .MuiInput-underline:before": {
+      borderBottom: `solid 1px ${theme.palette.primary[900]}`
     },
     "& .MuiInput-underline:after": {
-      color: theme.palette.primary[900]
+      borderBottom: `solid 2px ${theme.palette.primary[700]}`
     },
     "& .MuiInputBase-input": {
       color: theme.palette.primary[400]
     }
   }
 }))(TextField);
+
+// 'CSS'
 const styles = theme => {
   return {
     form: {
-      marginTop: 64,
+      marginTop: 8,
       display: "flex",
       flexDirection: "column",
       alignItems: "center",
@@ -29,13 +33,14 @@ const styles = theme => {
       padding: "16px",
       width: "288px",
       marginBottom: 8,
-      background: fade(darken(theme.palette.secondary[900], 0.3), 0.6),
+      background: fade(darken(theme.palette.secondary[900], 0.9), 0.6),
       borderRadius: "0px",
-      backdropFilter: "blur(5px)" // Only from chrome 76
+      backdropFilter: "blur(4px)" // Only from chrome 76
     },
     loginTitle: {
       color: theme.palette.primary[700],
-      textTransform: "uppercase"
+      textTransform: "uppercase",
+      letterSpacing: "2px"
     },
     textField: {
       marginBottom: "16px",
@@ -49,49 +54,187 @@ const styles = theme => {
       textShadow: "0px 1px 2px black",
       textTransform: "uppercase",
       cursor: "pointer",
-      color: theme.palette.primary[900],
+      color: theme.palette.primary[700],
       textAlign: "center"
+    },
+    error: {
+      textAlign: "center",
+      textTransform: "uppercase",
+      fontSize: "0.8em",
+      color: theme.palette.primary[300]
     }
   };
 };
 
-// TODO: Add text change listener make the text go somewhere.
 class LoginForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isRegister: false
+      inputNames: {
+        username: "username",
+        password: "password",
+        repassword: "repassword",
+        fname: "first name",
+        lname: "last name"
+      },
+      // If the inputNames are changed then change the keys below accordingly.
+      values: {
+        username: "",
+        password: "",
+        repassword: "",
+        "first name": "",
+        "last name": ""
+      },
+      isRegister: false,
+      error: ""
     };
     this.linkClickHandler = this.linkClickHandler.bind(this);
     this.submitClickHandler = this.submitClickHandler.bind(this);
+    this.inputChangeHandler = this.inputChangeHandler.bind(this);
+    this.inputValidator = this.inputValidator.bind(this);
+    this.loginValidator = this.loginValidator.bind(this);
+    this.registerValidator = this.registerValidator.bind(this);
+    this.postRegisterHandler = this.postRegisterHandler.bind(this);
+  }
+  inputValidator(input) {
+    if (input !== "") {
+      return true;
+    }
+    return false;
+  }
+  loginValidator() {
+    const { inputNames, values } = this.state;
+    const loginInputs = [inputNames.username, inputNames.password];
+    let valid = true;
+    loginInputs.forEach(i => {
+      if (!this.inputValidator(values[i])) {
+        this.setState({ error: `${i} is invalid` });
+        valid = false;
+      }
+    });
+    return valid;
+  }
+  registerValidator() {
+    const { inputNames, values } = this.state;
+    const registerInputs = [inputNames.password, inputNames.repassword, inputNames.lname, inputNames.fname];
+    let valid = true;
+    registerInputs.forEach(i => {
+      if (!this.inputValidator(values[i])) {
+        this.setState({ error: `${i} is invalid` });
+        valid = false;
+      }
+    });
+    if (values[inputNames.password] !== values[inputNames.repassword]) {
+      this.setState({ error: "passwords don't match" });
+      valid = false;
+    }
+    return valid;
+  }
+  inputChangeHandler({ target: { name, value } }) {
+    this.setState(({ values }) => {
+      values[name] = value;
+      return {
+        values: values
+      };
+    });
   }
   linkClickHandler() {
-    this.setState(({ isRegister }) => ({ isRegister: !isRegister }));
+    this.setState(({ isRegister }) => ({ isRegister: !isRegister, error: "" }));
+  }
+  postRegisterHandler() {
+    this.setState(({ values, isRegister }) => {
+      Object.keys(values).forEach(k => (values[k] = ""));
+      return { values, isRegister: !isRegister, error: "registered" };
+    });
   }
   submitClickHandler(e) {
     e.preventDefault();
-    alert("SUBMITTED");
+    const { isRegister } = this.state;
+    let valid = this.loginValidator();
+    if (isRegister && valid) {
+      valid = this.registerValidator();
+      if (valid) {
+        if (this.props.registerHandler(this.state.values)) {
+          this.postRegisterHandler();
+        } else {
+          valid = false;
+          this.setState({ error: "registeration failed" });
+        }
+      }
+    } else if (valid) {
+      if (!this.props.loginHandler(this.state.values)) {
+        this.setState({ error: "login failed" });
+      }
+    }
   }
   render() {
     const { classes } = this.props;
+    const { error, isRegister, inputNames, values } = this.state;
     return (
-      <div>
-        <Paper elevation={10} className={classes.form} id="login-form">
+      <div style={{ marginTop: 48 }}>
+        <Collapse in={error !== ""}>
+          {/* <Collapse in={true}> */}
+          <Typography className={classes.error}>{error}</Typography>
+        </Collapse>
+        <Paper component="form" elevation={10} className={classes.form} id="login-form">
           <Typography component="h3" className={classes.loginTitle}>
-            {this.state.isRegister ? "register" : "login"}
+            {isRegister ? "register" : "login"}
           </Typography>
-          <CustomTextField label="Username" fullWidth className={classes.textField} />
-          <CustomTextField label="Password" fullWidth type="password" className={classes.textField} />
-          <Collapse in={this.state.isRegister}>
-            <CustomTextField label="Re Password" fullWidth type="password" className={classes.textField} />
-            <CustomTextField label="Email" fullWidth type="email" className={classes.textField} />
+          <CustomTextField
+            autoComplete="off"
+            label="Username"
+            fullWidth
+            className={classes.textField}
+            name={inputNames.username}
+            value={values[inputNames.username]}
+            onChange={this.inputChangeHandler}
+          />
+          <CustomTextField
+            label="Password"
+            autoComplete="off"
+            fullWidth
+            type="password"
+            className={classes.textField}
+            name={inputNames.password}
+            value={values[inputNames.password]}
+            onChange={this.inputChangeHandler}
+          />
+          <Collapse in={isRegister}>
+            <CustomTextField
+              label="Re Password"
+              autoComplete="off"
+              fullWidth
+              type="password"
+              className={classes.textField}
+              name={inputNames.repassword}
+              value={values[inputNames.repassword]}
+              onChange={this.inputChangeHandler}
+            />
+            <CustomTextField
+              autoComplete="off"
+              label="First Name"
+              fullWidth
+              className={classes.textField}
+              name={inputNames.fname}
+              value={values[inputNames.fname]}
+              onChange={this.inputChangeHandler}
+            />
+            <CustomTextField
+              autoComplete="off"
+              label="Last Name"
+              fullWidth
+              className={classes.textField}
+              name={inputNames.lname}
+              value={values[inputNames.lname]}
+              onChange={this.inputChangeHandler}
+            />
           </Collapse>
-          <Button size="large" className={classes.button} onClick={this.submitClickHandler}>
+          <Button type="submit" size="large" className={classes.button} onClick={this.submitClickHandler}>
             submit
           </Button>
         </Paper>
         <Link className={classes.link} onClick={this.linkClickHandler}>
-          <Typography>{this.state.isRegister ? "login" : "register"}</Typography>
+          <Typography>{isRegister ? "login" : "register"}</Typography>
         </Link>
       </div>
     );
--- src/login/LoginPage.js
+++ src/login/LoginPage.js
@@ -17,19 +17,19 @@ const styles = theme => {
       color: theme.palette.primary[600],
       fontFamily: ["Righteous", "sans-serif"],
       letterSpacing: "10px",
-      fontSize:"6rem",
-      textShadow:"0px 1px 4px rgba(0,0,0,0.7)"
+      fontSize: "6rem",
+      textShadow: "0px 1px 4px rgba(0,0,0,0.7)"
     },
     subtitle: {
       color: theme.palette.primary[400],
       fontFamily: ["Roboto", "sans-serif"],
       fontWeight: 400,
       letterSpacing: "5px",
-      fontSize:"1.4rem",
-      textShadow:"0px 1px 4px rgba(0,0,0,0.7)"
+      fontSize: "1.4rem",
+      textShadow: "0px 1px 4px rgba(0,0,0,0.7)"
     },
     container: {
-      height:"100%",
+      height: "100%",
       display: "flex",
       justifyContent: "space-around",
       alignItems: "center"
@@ -41,6 +41,16 @@ class LoginPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+  }
+  loginHandler(values) {
+    // Add code to post login details.
+    console.log("logging in", values);
+    return false
+  }
+  registerHandler(values) {
+    // Add code to register user.
+    console.log("registering", values);
+    return false;
   }
   render() {
     const { classes } = this.props;
@@ -54,7 +64,7 @@ class LoginPage extends React.Component {
             The workout tracker.
           </Typography>
         </div>
-        <LoginForm />
+        <LoginForm loginHandler={this.loginHandler} registerHandler={this.registerHandler} />
       </div>
     );
   }
