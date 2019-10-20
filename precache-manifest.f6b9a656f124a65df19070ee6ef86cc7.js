89d4881967fdeb10b5da8dbdcc7ce34baac80396
18alantom <2.alan.tom@gmail.com>
2019-10-20 15:56:43 +0530

    Updates

---
+++ precache-manifest.f6b9a656f124a65df19070ee6ef86cc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fd442fc4a87294722ac16eca13316e7",
+    "url": "/fetzen/index.html"
+  },
+  {
+    "revision": "14666b764a31a96c3c7d",
+    "url": "/fetzen/static/css/main.2b2bce54.chunk.css"
+  },
+  {
+    "revision": "d41da6c5f8b77a2fff21",
+    "url": "/fetzen/static/js/2.1b0c2de2.chunk.js"
+  },
+  {
+    "revision": "14666b764a31a96c3c7d",
+    "url": "/fetzen/static/js/main.8aa4ec34.chunk.js"
+  },
+  {
+    "revision": "56d006ebc47859bdb60a",
+    "url": "/fetzen/static/js/runtime~main.5b2121bc.js"
+  },
+  {
+    "revision": "dee696b4eadc07015081365c08fc82dc",
+    "url": "/fetzen/static/media/bg_image_half_res.dee696b4.jpg"
+  }
+]);
